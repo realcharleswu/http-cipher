5903e14ac446cc7a330fcb364556978392a9ab23
Charles Wu <charlw5@uci.edu>
2024-07-31 17:26:28 -0700

    feat(decoder): Added flag to CLI

--- cli/main.go
+++ cli/main.go
@@ -1,36 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/url"
 )
 
 func main() {
-	fmt.Println("Enter a valid URL: ")
-	var user_input string
+	decode_mode := flag.Bool("d", false, "Enable debug mode")
+	flag.Parse()
+	if *decode_mode {
+		fmt.Println("Enter a valid URL: ")
+		var user_input string
 
-	_, err := fmt.Scan(&user_input)
-	if err != nil {
-		fmt.Println("User input error:", err)
-		return
-	}
-	user_url, err := url.Parse(user_input)
-	if err != nil {
-		fmt.Println("Parsing error", err)
-	}
-	if isValidURL(user_url) {
-		if !urlPathIsEmpty(user_url) {
-			decodedPath, err := url.QueryUnescape(user_url.Path)
-			if err != nil {
-				return
-			}
-			fmt.Printf("URL with path decoded:\n%s://%s%s\n", user_url.Scheme, user_url.Host, decodedPath)
-		} else {
-			fmt.Println("Path is empty, nothing to decode.")
+		_, err := fmt.Scan(&user_input)
+		if err != nil {
+			fmt.Println("User input error:", err)
+			return
 		}
-	} else {
-		fmt.Println("Invalid URL!")
-		return
+		user_url, err := url.Parse(user_input)
+		if err != nil {
+			fmt.Println("Parsing error", err)
+		}
+		if isValidURL(user_url) {
+			if !urlPathIsEmpty(user_url) {
+				decodedPath, err := url.QueryUnescape(user_url.Path)
+				if err != nil {
+					return
+				}
+				fmt.Printf("URL with path decoded:\n%s://%s%s\n", user_url.Scheme, user_url.Host, decodedPath)
+			} else {
+				fmt.Println("Path is empty, nothing to decode.")
+			}
+		} else {
+			fmt.Println("Invalid URL!")
+			return
+		}
 	}
 
 }
